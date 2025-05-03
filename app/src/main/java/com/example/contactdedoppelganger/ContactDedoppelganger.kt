1cb885c6f258ef015f42f9bce37d94362d9edf0c
OwletsFox <Aristark-Inferno@yandex.ru>
2025-05-03 16:25:42 +0300

    hilt integration and skeleton

---
+++ app/src/main/java/com/example/contactdedoppelganger/ContactDedoppelganger.kt
@@ -0,0 +1,8 @@
+package com.example.contactdedoppelganger
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ContactDedoppelganger : Application() {
+}
--- app/src/main/java/com/example/contactdedoppelganger/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/contactdedoppelganger/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.contactdedoppelganger
+package com.example.contactdedoppelganger.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.contactdedoppelganger.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
